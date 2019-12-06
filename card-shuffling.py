a7bd917342e2b41bcd3867834db9243a05201bf2
TheArchitect326 <seftontycho@hotmail.co.uk>
2019-12-06 23:11:05 +0000

    Update card-shuffling.py

--- card-shuffling.py
+++ card-shuffling.py
@@ -46,10 +46,12 @@ def guessing(after, before=list(range(52))):
     count, guess, lastcard, toguess = 0, 0, 0, [i for i in before]
 
     for card in after:
-        if before[before.index(lastcard) + 1] not in toguess:
-            guess = numpy.random.choice(toguess)
-        else:
-            guess = before[before.index(lastcard) + 1]
+        guess = numpy.random.choice(toguess)
+
+        if lastcard != before[-1]:
+            if before[before.index(lastcard) + 1] in toguess:
+                guess = before[before.index(lastcard) + 1]
+                                
 
         if card == guess:
             count += 1
@@ -84,15 +86,34 @@ def getdata(shuffle=rand, measure=RSS,  decks=1000, shuffles=1, accuracy=1):
         json.dump(ydata, outfile)
 
 
-for shuff in (rand, riffle, overhand)[2:]:
-    print(shuff.__name__)
-    for mesr in (RSS, RMS, guessing):
-        getdata(shuffle=shuff, measure=mesr, decks=1000000, shuffles=1, accuracy=2)
-        print('Got nice data for ' + shuff.__name__ + ' ' + mesr.__name__)
+def fastdata(shuffles=(rand, riffle, overhand), measures=(RSS, guessing), decks=1000, iters=1, accuracy=1):
+    for i, shuff in enumerate(shuffles):
+        newdecks, olddecks = [], [list(range(52)) for _ in range(decks)]
 
-        if shuff == rand or mesr == RMS or (shuff == riffle and mesr == RSS):
-            continue
+        xdata = [[[] for _ in range(iters)] for __ in range(len(measures))]
 
-        getdata(shuffle=shuff, measure=mesr, decks=100000, shuffles=20, accuracy=1)
-        print('Got long term data for ' + shuff.__name__ + ' ' + mesr.__name__)
+        for j in range(iters):
+            print('Shuffling ' + str(decks) + ' decks...')
+            newdecks = [shuff(deck) for deck in olddecks]
 
+            for k in range(decks):
+                print('Deck: ' + str(k))
+                for c, mesr in enumerate(measures):
+                    xdata[c][j].append(round(mesr(newdecks[k], olddecks[k]), accuracy))
+
+        currentx = [[list(dict.fromkeys(iteration)) for iteration in mesr] for mesr in xdata]
+        ydata = [[[xdata.count(data) for data in iteration] for iteration in mesr] for mesr in currentx]
+
+        for c, mesr in enumerate(measures):
+            with open('_'.join([shuff.__name__, mesr.__name__, str(decks), str(iters), str(accuracy), 'x.txt']), 'w') as outfile:
+                json.dump(xdata[c], outfile)
+
+            with open('_'.join([shuff.__name__, mesr.__name__, str(decks), str(iters), str(accuracy), 'y.txt']), 'w') as outfile:
+                json.dump(ydata[c], outfile)
+
+
+fastdata(decks=1000, accuracy=2)
+print('Got nice data')
+
+fastdata(decks=1000, iters=20, accuracy=1)
+print('Got long term data')
